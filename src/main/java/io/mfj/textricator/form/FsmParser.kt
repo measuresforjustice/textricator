measuresforjustice/textricator

commit 6c27351ad6c8676ebab997c7abb2c4b5606b1c84

Add State.skip. Fix pageNumberTransition bug.

src/main/java/io/mfj/textricator/form/FsmParser.kt
@@ -119,8 +119,26 @@ class FsmParser(val config:FormParseConfig,
         // group by state (run the FSM)
         .plus( null as Text? ) // sentinel
         .map { text -> parse( parseState, text ) }
-        .filter { it != null }.map { it!! }
+        .filterNotNull()
+        // remove skipped states and combine now-adjacent siblings of the same state
+        .filter { sv -> ! sv.state.skip }.combine()
+  }
 
+  /**
+   * Combine adjacent siblings with the same state.
+   */
+  private fun Sequence<StateValue>.combine():Sequence<StateValue> = sequence {
+    var last:StateValue? = null
+    forEach { sv ->
+      last = when {
+        last == null -> sv
+        last!!.state == sv.state -> last!!.copy( values = last!!.values + sv.values )
+        else -> {
+          yield(last!!)
+          sv
+        }
+      }
+    }
   }
 
   private class ParseState( private val config:FormParseConfig, private val eventListener:FsmEventListener? ) {
@@ -245,6 +263,7 @@ class FsmParser(val config:FormParseConfig,
         null
       }
       buffer.clear()
+      last = null
 
       return stateTexts
     }
@@ -349,9 +368,15 @@ class FsmParser(val config:FormParseConfig,
       // state change
 
       if ( ret != null ) {
-        // Somehow a pageNumber transition happened, and texts were already added.
-        // Texts should not have been added.
-        throw Exception( "BUG" )
+        // pageNumber transition happened
+        if ( s.lastText != null ) {
+          // texts were already added.
+          // Texts should not have been added.
+          throw Exception( "BUG" )
+        } else {
+          // just happened, the new page state had nothing added to it. That's fine.
+          ret = null
+        }
       }
 
       eventListener?.onStateChange(text.pageNumber, nextStateId)

src/main/java/io/mfj/textricator/form/StateValue.kt
@@ -22,5 +22,5 @@ import io.mfj.textricator.record.Value
 /**
  * @param pageNumber The pageNumber that the state started on.
  */
-class StateValue( val pageNumber:Int, val stateId:String, val state:State, val values:List<Value>,
+data class StateValue( val pageNumber:Int, val stateId:String, val state:State, val values:List<Value>,
     val splitContinuation:Boolean=false )

src/main/java/io/mfj/textricator/form/config/State.kt
@@ -23,6 +23,10 @@ import java.util.*
  */
 
 data class State(
+    /** If true, remove text in this state before record processing. */
+    val skip:Boolean = false,
+    /** If false, do not create records for this state, but still include it in record processing
+     * so it can split adjacent states and affect if the next sibling starts a record. */
     val include:Boolean = true,
     val transitions: MutableList<Transition> = ArrayList(),
     val startRecord: Boolean = false,

commit b706ff94091fc1a4938fe7f32ced9c6e33626311

Fix page number transition bug introduced in 6c27351
It was losing the last StateValue on each page.

src/main/java/io/mfj/textricator/form/FsmParser.kt
@@ -374,14 +374,19 @@ class FsmParser(val config:FormParseConfig,
           // Texts should not have been added.
           throw Exception( "BUG" )
         } else {
-          // just happened, the new page state had nothing added to it. That's fine.
-          ret = null
+          // PageNumber transition just happened, the new page state had nothing added to it.
+          // That's fine.
+          // Set the new state, but ignore the returned StateValue because it is empty
+          // (because of the page transition).
+          s.setState( nextStateId )
+          s.addText()
         }
+      } else {
+        eventListener?.onStateChange(text.pageNumber, nextStateId)
+        ret = s.setState( nextStateId )
+        s.addText()
       }
 
-      eventListener?.onStateChange(text.pageNumber, nextStateId)
-      ret = s.setState( nextStateId )
-      s.addText()
     }
 
     return ret